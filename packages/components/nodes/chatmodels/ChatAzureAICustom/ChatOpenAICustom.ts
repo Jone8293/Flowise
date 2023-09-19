e5b3b8b89253a3d97d8d385a3a4f28e4a751d0bf
Jone8293 <58243569+Jone8293@users.noreply.github.com>
2023-09-19 17:32:41 +0100

    Update ChatOpenAICustom.ts

--- packages/components/nodes/chatmodels/ChatAzureAICustom/ChatOpenAICustom.ts
+++ packages/components/nodes/chatmodels/ChatAzureAICustom/ChatOpenAICustom.ts
@@ -1,50 +1,35 @@
-import { ICommonObject, INode, INodeData, INodeParams } from '../../../src/Interface'
-import { getBaseClasses, getCredentialData, getCredentialParam } from '../../../src/utils'
-import { ChatOpenAI, OpenAIChatInput } from 'langchain/chat_models/openai'
+import { ICommonObject, INode, INodeData, INodeParams } from '../../../src/Interface';
+import { getBaseClasses, getCredentialData, getCredentialParam } from '../../../src/utils';
+import { AzureMLChatOnlineEndpoint } from 'langchain/chat_models/azureml_endpoint';
 
-class ChatOpenAICustom_ChatModels implements INode {
-    label: string
-    name: string
-    version: number
-    type: string
-    icon: string
-    category: string
-    description: string
-    baseClasses: string[]
-    credential: INodeParams
-    inputs: INodeParams[]
+class AzureMLChatModels implements INode {
+    label: string;
+    name: string;
+    version: number;
+    type: string;
+    icon: string;
+    category: string;
+    description: string;
+    baseClasses: string[];
+    credential: INodeParams;
+    inputs: INodeParams[];
 
     constructor() {
-        this.label = 'ChatOpenAI Custom'
-        this.name = 'chatOpenAICustom'
-        this.version = 1.0
-        this.type = 'ChatOpenAI-Custom'
-        this.icon = 'openai.png'
-        this.category = 'Chat Models'
-        this.description = 'Custom/FineTuned model using OpenAI Chat compatible API'
-        this.baseClasses = [this.type, ...getBaseClasses(ChatOpenAI)]
+        this.label = 'AzureMLChatOnlineEndpoint';
+        this.name = 'azureMlChatOnlineEndpoint';
+        this.version = 1.0;
+        this.type = 'AzureMLChatOnlineEndpoint';
+        this.icon = 'azureMlChatOnlineEndpoint.png';
+        this.category = 'Chat Models';
+        this.description = 'Wrapper around AzureML large language models that use the Chat endpoint';
+        this.baseClasses = [this.type, ...getBaseClasses(AzureMLChatOnlineEndpoint)];
         this.credential = {
             label: 'Connect Credential',
             name: 'credential',
             type: 'credential',
-            credentialNames: ['openAIApi'],
-            optional: true
-        }
+            credentialNames: ['azureMlApi']
+        };
         this.inputs = [
-            {
-                label: 'Model Name',
-                name: 'modelName',
-                type: 'string',
-                placeholder: 'ft:gpt-3.5-turbo:my-org:custom_suffix:id'
-            },
-            {
-                label: 'Temperature',
-                name: 'temperature',
-                type: 'number',
-                step: 0.1,
-                default: 0.9,
-                optional: true
-            },
             {
                 label: 'Max Tokens',
                 name: 'maxTokens',
@@ -54,48 +39,19 @@ class ChatOpenAICustom_ChatModels implements INode {
                 additionalParams: true
             },
             {
-                label: 'Top Probability',
+                label: 'Temperature',
+                name: 'temperature',
+                type: 'number',
+                step: 0.1,
+                default: 0.6,
+                optional: true
+            },
+            {
+                label: 'Top P',
                 name: 'topP',
                 type: 'number',
                 step: 0.1,
-                optional: true,
-                additionalParams: true
-            },
-            {
-                label: 'Frequency Penalty',
-                name: 'frequencyPenalty',
-                type: 'number',
-                step: 0.1,
-                optional: true,
-                additionalParams: true
-            },
-            {
-                label: 'Presence Penalty',
-                name: 'presencePenalty',
-                type: 'number',
-                step: 0.1,
-                optional: true,
-                additionalParams: true
-            },
-            {
-                label: 'Timeout',
-                name: 'timeout',
-                type: 'number',
-                step: 1,
-                optional: true,
-                additionalParams: true
-            },
-            {
-                label: 'BasePath',
-                name: 'basepath',
-                type: 'string',
-                optional: true,
-                additionalParams: true
-            },
-            {
-                label: 'BaseOptions',
-                name: 'baseOptions',
-                type: 'json',
+                default: 0.9,
                 optional: true,
                 additionalParams: true
             }
@@ -103,48 +59,27 @@ class ChatOpenAICustom_ChatModels implements INode {
     }
 
     async init(nodeData: INodeData, _: string, options: ICommonObject): Promise<any> {
+        const endpointUrl = nodeData.inputs?.endpointUrl as string
+        const endpointApiKey = nodeData.inputs?.endpointApiKey as string
         const temperature = nodeData.inputs?.temperature as string
-        const modelName = nodeData.inputs?.modelName as string
         const maxTokens = nodeData.inputs?.maxTokens as string
         const topP = nodeData.inputs?.topP as string
-        const frequencyPenalty = nodeData.inputs?.frequencyPenalty as string
-        const presencePenalty = nodeData.inputs?.presencePenalty as string
-        const timeout = nodeData.inputs?.timeout as string
-        const streaming = nodeData.inputs?.streaming as boolean
-        const basePath = nodeData.inputs?.basepath as string
-        const baseOptions = nodeData.inputs?.baseOptions
 
         const credentialData = await getCredentialData(nodeData.credential ?? '', options)
-        const openAIApiKey = getCredentialParam('openAIApiKey', credentialData, nodeData)
+        const azureMlEndpointUrl = getCredentialParam('azureMlEndpointUrl', credentialData, nodeData)
+        const azureMlEndpointApiKey = getCredentialParam('azureMlEndpointApiKey', credentialData, nodeData)
 
-        const obj: Partial<OpenAIChatInput> & { openAIApiKey?: string } = {
+        const obj: Partial<AzureMLChatOnlineEndpoint> & { azureMlEndpointUrl?: string; azureMlEndpointApiKey?: string } = {
+            endpoint_url: azureMlEndpointUrl,
+            endpoint_api_key: azureMlEndpointApiKey,
             temperature: parseFloat(temperature),
-            modelName,
-            openAIApiKey,
-            streaming: streaming ?? true
-        }
+            maxTokens: parseInt(maxTokens, 10),
+            top_p: parseFloat(topP)
+        };
 
-        if (maxTokens) obj.maxTokens = parseInt(maxTokens, 10)
-        if (topP) obj.topP = parseFloat(topP)
-        if (frequencyPenalty) obj.frequencyPenalty = parseFloat(frequencyPenalty)
-        if (presencePenalty) obj.presencePenalty = parseFloat(presencePenalty)
-        if (timeout) obj.timeout = parseInt(timeout, 10)
-
-        let parsedBaseOptions: any | undefined = undefined
-
-        if (baseOptions) {
-            try {
-                parsedBaseOptions = typeof baseOptions === 'object' ? baseOptions : JSON.parse(baseOptions)
-            } catch (exception) {
-                throw new Error("Invalid JSON in the ChatOpenAI's BaseOptions: " + exception)
-            }
-        }
-        const model = new ChatOpenAI(obj, {
-            basePath,
-            baseOptions: parsedBaseOptions
-        })
-        return model
+        const model = new AzureMLChatOnlineEndpoint(endpoint_url, endpoint_api_key, Content_Formatter: LlamaContentFormatter);
+        return model;
     }
 }
 
-module.exports = { nodeClass: ChatOpenAICustom_ChatModels }
+module.exports = { nodeClass: AzureMLChatModels }
